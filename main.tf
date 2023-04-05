78c51c3b8daccba9dea17b798f828bb51ecc7392
ccseyhan <cemalcan.seyhan@deop.ca>
2023-04-05 14:34:35 -0300

    acr_password added

--- main.tf
+++ main.tf
@@ -53,5 +53,7 @@ resource "github_actions_secret" "example_secret" {
     azurerm_container_registry.acr
   ]
 }
-
+ output "acr_password" {
+  value = azurerm_container_registry.acr.admin_password
+ }
 

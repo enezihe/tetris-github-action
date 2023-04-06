0936966934cb3babde36ebd6d6c8eb7a8f1eea4e
ccseyhan <cemalcan.seyhan@deop.ca>
2023-04-06 14:38:04 -0300

    providers added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,16 @@ provider "github" {
 
 
 terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.50.0"
+    }
+     github = {
+      source = "integrations/github"
+      version = "5.20.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "sshkey"
     storage_account_name = "ccseyhan"

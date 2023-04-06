ae120cdadd2b7152f6658346ab4d1fc0f6a09aa3
ccseyhan <cemalcan.seyhan@deop.ca>
2023-04-06 12:53:20 -0300

    variables added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg1.name
   location            = var.location
-  sku                 = "Standart"
+  sku                 = var.acr_sku
   admin_enabled       = true
 }
 
